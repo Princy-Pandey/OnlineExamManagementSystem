0b7ff68e470c2aa6ddc8b3e621f76bcc76778a75
Princy Pandey <60910695+Princy-Pandey@users.noreply.github.com>
2020-09-20 00:19:36 +0530

    Packages and Entity Class Created

---
+++ OnlineExamManagementSystem/src/main/java/com/capgemini/onlineexammanegmentsystem/OnlineExamManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.onlineexammanegmentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineExamManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineExamManagementSystemApplication.class, args);
+	}
+
+}
---
+++ OnlineExamManagementSystem/src/test/java/com/capgemini/onlineexammanegmentsystem/OnlineExamManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.onlineexammanegmentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineExamManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
